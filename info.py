74d8e653e44232993baaa48526244ffaa6de1d97
iloveumedamm <140915401+iloveumedamm@users.noreply.github.com>
2024-03-08 18:17:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,8 +25,8 @@ PICS = (environ.get('PICS', 'https://telegra.ph/file/65fe86fc02a73f6fcf0ce.jpg h
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/85d361ab4cb6511006022.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/86b7b7e2aa7e38f328902.jpg")
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/b2b7504687ec41b794b9a.jpg'))
-CODE = (environ.get('CODE', 'https://graph.org/file/b2b7504687ec41b794b9a.jpg'))
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/24066cf7e315e12d3ac38.jpg'))
+CODE = (environ.get('CODE', 'https://graph.org/file/24066cf7e315e12d3ac38.jpg'))
 
 #stream link shortner
 STREAM_SITE = (environ.get('STREAM_SITE', 'bzearn.com'))
